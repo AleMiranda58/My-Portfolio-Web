0c8a0eda75e65f907526892d48b3170d1c952db8
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-22 12:31:12 -0500

    feat: reusable section component

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLline: 'Premium Bank',
+    headline: "asdfasdfasdfadfdfadfaf",
+    description: 'yuyuyuyuyyuyyu',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/undraw.svg'),
+    alt: 'email',
+    dark: true,
+    primary: true,
+    darkText: false
+}
