61d5a774c67494e3865e3f975aaefe1a9b3b03dd
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-22 16:47:33 -0500

    style: change nav bar color on scrolling

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaBars} from 'react-icons/fa'
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, SocialContainer,  SocialMedia} from './NavbarElements'
 
 const Navbar = ( { toggle }) => {
+    const [scrollNav, setScrollNav] = useState(false)
+
+    const changeNav = () => {
+        if(window.scrollY >= 80){
+            setScrollNav(true)
+        }else{
+            setScrollNav(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
     return (
         <>
-        <Nav>
+        <Nav scrollNav={scrollNav}>
             <NavbarContainer>
                 <NavLogo to='/'>Portfolio</NavLogo>
                 <MobileIcon onClick={toggle}>
