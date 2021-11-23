d631a77cfe8bfe507b972d80f4d01a979a2322e4
Alejandra Miranda <ale.miranda890@gmail.com>
2021-11-23 14:47:52 -0500

    style: working on fixing details of project section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import Project from '../Project/Project'
 import './projects.scss'
 import { projects } from '../../data'
+import { Button } from '../Button/buttonElements'
 
 const Projects = () => {
     return (
         <div className="project-section" id="projects">
             <div className="intro-section">
                 <h1>My projects</h1>
-                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis felis at mi pharetra fermentum. Vivamus a felis non arcu fermentum commodo quis at nisi. Aliquam cursus scelerisque lorem at auctor. Suspendisse dictum quis sem a maximus. Curabitur sodales tincidunt eleifend. Sed vitae risus feugiat, feugiat lectus eu, bibendum urna. Suspendisse vel facilisis augue, eu volutpat dolor.</p>
+                <p className="description">Lorem ipsum dolor sit amet consectetur adipiscing elit.haretra fermentum. Vivamus a felis non arcu fermentum commodo quis at nisi. Aliquam cursus scelerisque lorem at auctor. Suspendisse dictum quis sem a maximus. Curabitur sodales tincidunt eleifend. Sed vitae risus feugiat, feugiat lectus eu, bibendum urna. Suspendisse vel facilisis augue, eu volutpat.</p>
+                {/* <Button>See codes source</Button> */}
             </div>
             <div className="project-list">
                 { projects.map((item)=> (
-                <Project key={item.id} img={item.img} link={item.link} />
+                <Project key={item.id} img={item.img} link={item.link} description={item.description}/>
                 ))}
+            </div>
+            <div className="btn">
+            <Button>See more Projects</Button>
             </div>
         </div>
     )
--- src/data.js
+++ src/data.js
@@ -2,12 +2,14 @@ export const projects = [
     {
     id: 1,
     img: "https://i.ibb.co/ctQ1bYH/testing-Full-Img.png",
-    link: "https://github.com/AleMiranda58"
+    link: "https://github.com/AleMiranda58",
+    description: "E-commerce developed with React, Redux, Striple, etc"
     },
     {
     id: 2,
     img: "https://i.ibb.co/ctQ1bYH/testing-Full-Img.png",
-    link: "https://github.com/AleMiranda58"
+    link: "https://github.com/AleMiranda58",
+    description: "jQuery website, SASS, Email.js, third-party library"
     },
     {
     id: 3,
