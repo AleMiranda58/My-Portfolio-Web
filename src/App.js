5ec7c44b0f323dc4c010b9442ea0c66bba95cacf
Alejandra Miranda <ale.miranda890@gmail.com>
2021-12-03 01:31:32 -0500

    style: modif links for the fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
-// import Intro from './components/Intro/Intro';
-// import Projects from './components/Projects/Projects';
-// import Contact from './components/contact/Contact';
 import './App.css'
 import Home from './pages';
 
